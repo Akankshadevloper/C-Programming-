85c9d08b3c1241c6fc2e29ff87ffaa23b20cdc9e
Akanksha Kumari <200301120008@cutm.ac.in>
2024-08-04 15:17:43 +0530

    file added

---
+++ output/GradeOfStudent.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int marks;
+
+    printf("Enter your marks: ");
+    scanf("%d", &marks);
+
+    if (marks >= 90) {
+        printf("Grade: A\n");
+    } else if (marks >= 80) {
+        printf("Grade: B\n");
+    } else if (marks >= 70) {
+        printf("Grade: C\n");
+    } else if (marks >= 60) {
+        printf("Grade: D\n");
+    } else {
+        printf("Grade: F\n");
+    }
+
+    return 0;
+}
