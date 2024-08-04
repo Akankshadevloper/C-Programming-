336284d086739a2d4310540381aacf48d2f262c6
Akanksha Kumari <200301120008@cutm.ac.in>
2024-08-04 14:59:30 +0530

    file added

---
+++ output/LargestOfThree.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a >= b && a >= c) {
+        printf("%d is the largest number.\n", a);
+    } else if (b >= a && b >= c) {
+        printf("%d is the largest number.\n", b);
+    } else {
+        printf("%d is the largest number.\n", c);
+    }
+
+    return 0;
+}
