100b51f400e598755fd487360838a7392c0da4a4
Akanksha Kumari <200301120008@cutm.ac.in>
2024-08-08 11:06:09 +0530

    file added

---
+++ Palindrome.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num, original, reversed = 0, remainder;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    if (original == reversed) {
+        printf("%d is a palindrome.\n", original);
+    } else {
+        printf("%d is not a palindrome.\n", original);
+    }
+
+    return 0;
+}
