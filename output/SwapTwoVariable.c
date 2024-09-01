e91d40ecf03978453fdf3f36f45dc16c2f2b0e5a
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-01 15:51:00 +0530

    file added

---
+++ output/SwapTwoVariable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swapping: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
