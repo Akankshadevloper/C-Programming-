123b6cdcfe4c2874f38ff64db48fce952241e756
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-02 19:22:38 +0530

    file added

---
+++ output/Recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    printf("Factorial of %d is %d\n", number, factorial(number));
+
+    return 0;
+}
